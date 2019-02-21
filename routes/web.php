5274e187f5d6aa5772bfe4e973f0876cf290aa87
toshikish <toshiki@nanshika.com>
2019-02-22 01:35:38 +0900

    Add threads.index and threads.show

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/{thread}', 'ThreadsController@show');
